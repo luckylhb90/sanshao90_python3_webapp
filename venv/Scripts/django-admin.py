65501af57cbbe177bf470d2e05c3fa81c82b505a
lucky <luckylhb90@gmail.com>
2017-12-09 19:56:05 +0800

    init

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\workspace\github\python\sanshao90_python3_webapp\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
